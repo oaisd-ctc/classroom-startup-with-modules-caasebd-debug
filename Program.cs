94ad54e7bfd60b406f2d86178e88df1cbbf81302
Noah-Castaneda <caasebd@student.oaisd.org>
2025-10-27 14:44:57 +0000

    Looks good!

--- Program.cs
+++ Program.cs
@@ -2,56 +2,151 @@
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static void Main(string[] args, char s)
     {
-        FindSeat();
-        SignIn();
-        DailyLogic();
-        BoxBreathing(5);
-        DailyHypothetical();
+        FahrenheitToCelsius(68); // Test FahrenheitToCelsius Function with a random value
+
+        // Test your functions by calling them below:
+        CheckForPositiveNegativeZero(3);
+        FindMinimum(45, 15, 50);
+        FindMaximum(45, 15, 50);
+        IsDivisibleBy5(30);
+        CheckEvenOrOdd(9);
+        CheckVowelOrConsonant(s);
+        DisplayDayOfWeek(5);
+
+        // CheckForPositiveNegativeZero(-1); // This should output: "Your number is negative"
+        // CheckForPositiveNegativeZero(0); // This should output: "Your number is zero"
+        // CheckForPositiveNegativeZero(1); // This should output: "Your number is positive"
+        // ...
+        // ...
+        // DisplayDayOfWeek(0); // This should output: "Thursday"
+
     }
 
-    public static void FindSeat()
+    public static void FahrenheitToCelsius(double fahrenheit)
     {
-        Console.WriteLine("Walk in the room.");
-        Console.WriteLine("Walk towards desk.");
-        Console.WriteLine("Pull out chair.");
-        Console.WriteLine("Take off backpack.");
-        Console.WriteLine("Place backpack on the ground.");
-        Console.WriteLine("Sit down in chair.");
+        double celsiusDegree = (fahrenheit - 32) * 5 / 9;
+        Console.WriteLine($"{fahrenheit} degrees Fahrenheit is {celsiusDegree} degrees Celsius.");
     }
 
-    public static void SignIn()
+    // Define your functions below:
+    public static void CheckForPositiveNegativeZero(int number)
     {
-        Console.WriteLine("Turn on computer.");
-        Console.WriteLine("Sign in to computer.");
+        //Determine if parameter is negative, positive or zero
+        if (number <= -1)
+        {
+            Console.WriteLine("Your number is negative");
+        }
+        else if (number == 0)
+        {
+            Console.WriteLine("Your number is zero");
+        }
+        else if (number >= 1)
+        {
+            Console.WriteLine("Your number is positive");
+        }
     }
 
-    public static void DailyLogic()
+    public static void FindMinimum(int FindMinimum, int v, int f)
     {
-        Console.WriteLine("Open browser.");
-        Console.WriteLine("Navigate to the Daily Logic classroom.");
-        Console.WriteLine("Complete the Daily Logic for the day.");
+        object value = FindMinimum <<min(FindMinimum);
+			{
+				Console.WriteLine("Minimum is min");
+			}
     }
 
-
-    public static void BoxBreathing(int duration)
+    private static int min(int findMinimum)
     {
-        Console.WriteLine("Inhale for " + duration + " seconds.");
-        Console.WriteLine("Hold breath for " + duration + " seconds.");
-        Console.WriteLine("Exhale for " + duration + " seconds.");
-        Console.WriteLine("Hold breath for " + duration + " seconds.");
+        throw new NotImplementedException();
     }
 
-    public static void DailyHypothetical()
+    public static void FindMaximum(int FindMaximum, int v, int f)
     {
-        Console.WriteLine("Open a new tab in the browser.");
-        Console.WriteLine("Navigate to Google.");
-        Console.WriteLine("Open Gmail.");
-        Console.WriteLine("Read Daily Hypothetical.");
-        Console.WriteLine("Talk to neighbors about Daily Hypothetical.");
-        Console.WriteLine("Respond to Daily Hypothetical email.");
+        object value = FindMaximum <<max(FindMaximum);
+		{
+			Console.WriteLine("Maximum is max");
+		}
     }
 
+    private static int max(int findMaximum)
+    {
+        throw new NotImplementedException();
+    }
 
+    public static void IsDivisibleBy5(int number)
+    {
+        //Check if divisible by 5
+        if (number % 5 == 0)
+        {
+            Console.WriteLine("Number is divisible by 5!");
+        }
+        else if (number != 5)
+        {
+            Console.WriteLine("Number is not divisible by 5.");
+        }
+    }
+
+    public static void CheckEvenOrOdd(int num)
+    {
+        bool IsEven = (num % 2 == 0);
+	if(num%2 == 0)
+	{
+        	Console.WriteLine("Number is even");
+	}
+	else
+	{
+		Console.WriteLine("Number is odd");
+	}
+    }
+
+    public static void CheckVowelOrConsonant(char s)
+    {
+		char letter = s;
+		switch (letter)
+		{
+			case 'a':
+			case 'A':
+			case 'c':
+			case 'C':
+			case 's':
+			case 'S':
+			case 'y':
+			case 'Y':
+				System.Console.WriteLine("Vowel");
+				break;
+			default:
+				System.Console.WriteLine("Not a vowel");
+				break;
+		}
+    }
+    
+    public static void DisplayDayOfWeek(int daycode)
+    {
+      int day = 4;
+      switch (day) 
+      {
+        case 1:
+          Console.WriteLine("Monday");
+          break;
+        case 2:
+          Console.WriteLine("Tuesday");
+          break;
+        case 3:
+          Console.WriteLine("Wednesday");
+          break;
+        case 4:
+          Console.WriteLine("Thursday");
+          break;
+        case 5:
+          Console.WriteLine("Friday");
+          break;
+        case 6:
+          Console.WriteLine("Saturday");
+          break;
+        case 7:
+          Console.WriteLine("Sunday");
+          break;
+    	}
+	}
 }
